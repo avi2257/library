bf0b6a79d83769d1462260e92f69ff92f3ad1e4d
Aviral <aviral6798@gmail.com>
2023-03-17 14:23:17 +0530

    initial commit

---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="center">
+        <v-row>
+            <v-col cols="10" offset="1">
+                <v-avatar color="primary" size="56" tile>
+                    <v-img src="@/assets/logo.png"></v-img>
+                </v-avatar>
+                <v-text-field label="Username" v-model="username"></v-text-field>
+                <v-text-field label="password" type="password" v-model="password"></v-text-field>
+                <v-btn class="base-inverted" block @click="authenticate()">Login</v-btn>
+            </v-col>
+        </v-row>
+        <h1>Login page</h1>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {    
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        authenticate() {
+            alert("Login")
+            console.log(this.username, this.password)
+        }
+    }
+}
+</script>
+    
+<style lang="scss" scoped></style>
